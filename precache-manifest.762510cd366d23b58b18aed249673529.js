d8d121045a58b9dc6137145100a7bf94958d8d3a
Simon X <simonxiong11@gmail.com>
2019-01-22 12:48:06 -0800

    Updates

---
+++ precache-manifest.762510cd366d23b58b18aed249673529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/film-freeze/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "005e033ffd605396749d",
+    "url": "/film-freeze/static/js/runtime~main.005e033f.js"
+  },
+  {
+    "revision": "9d32fed6670797ff999e",
+    "url": "/film-freeze/static/js/main.9d32fed6.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/film-freeze/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9d32fed6670797ff999e",
+    "url": "/film-freeze/static/css/main.eaa05421.chunk.css"
+  },
+  {
+    "revision": "03a1c1be588c548d15ab00e1e1d914fd",
+    "url": "/film-freeze/index.html"
+  }
+];
