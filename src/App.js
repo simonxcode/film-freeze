4f01f65fa89415b003a6b1687780610a0e8b82bd
Simon X <simonxiong11@gmail.com>
2019-01-27 13:35:29 -0800

    set up controlled component for input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,93 @@
 import React, { Component } from 'react';
 
 class Question extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedOption: ''
+    };
+  }
+
+  handleOptionChange = changeEvent => {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+  }
+
   render() {
     return (
-      <p>Greetings from Question component</p>
+      <div>
+        <form>
+          <div className='form-check'>
+            <label>
+              <input
+                type='radio'
+                name='answerOption'
+                value='jackieBrown'
+                checked={this.state.selectedOption === 'jackieBrown'}
+                onChange={this.handleOptionChange}
+                className='form-check-input'
+              />
+              Jackie Brown
+            </label>
+          </div>
+
+          <div className='form-check'>
+            <label>
+              <input
+                type='radio'
+                name='answerOption'
+                value='killBill'
+                checked={this.state.selectedOption === 'killBill'}
+                onChange={this.handleOptionChange}
+                className='form-check-input'
+              />
+              Kill Bill
+            </label>
+          </div>
+
+          <div className='form-check'>
+            <label>
+              <input
+                type='radio'
+                name='answerOption'
+                value='hatefulEight'
+                checked={this.state.selectedOption === 'hatefulEight'}
+                onChange={this.handleOptionChange}
+                className='form-check-input'
+              />
+              Hateful Eight
+            </label>
+          </div>
+
+          <div className='form-check'>
+            <label>
+              <input
+                type='radio'
+                name='answerOption'
+                value='pulpFiction'
+                checked={this.state.selectedOption === 'pulpFiction'}
+                onChange={this.handleOptionChange}
+                className='form-check-input'
+              />
+              Pulp Fiction
+            </label>
+          </div>
+        </form>
+      </div>
     );
   }
 }
+
+// class Answer extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <p>This is from the Answer Component</p>
+//       </div>
+//     )
+//   }
+// }
+
 
 export default Question;
