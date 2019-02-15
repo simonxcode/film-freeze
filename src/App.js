d0820e91cba73f1bab9e0113b1168e95506da169
Simon X <simonxiong11@gmail.com>
2019-02-14 20:33:54 -0800

    create a test component with Array Iterator logic and onClick event to display a new movie title per each click

--- src/App.js
+++ src/App.js
@@ -1,103 +1,138 @@
 import React, { Component } from 'react';
 
-//child component
-class Question extends Component {
+
+// // child component
+// class Question extends Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       selectedOption: ''
+//     };
+//   }
+
+//   // function to update current state
+//   handleOptionChange = changeEvent => {
+//     this.setState({
+//       selectedOption: changeEvent.target.value
+//     });
+//   }
+
+//   // function to submit change state and print answer result 
+//   handleFormSubmit = formSubmitEvent => {
+//     formSubmitEvent.preventDefault(); 
+//     if(this.state.selectedOption === 'pulpFiction') {
+//       return console.log('correct')
+//     } else {
+//       return console.log('incorrect')
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <div>
+//           <img className='imageStill' src='https://i2.wp.com/film-grab.com/wp-content/uploads/2011/02/223.png' alt='Pulp Fiction'></img>
+//         </div>
+//           <form onSubmit={this.handleFormSubmit}> 
+//             <div className='form-check'>
+//               <label>
+//                 <input
+//                   type='radio'
+//                   name='answerOption'
+//                   value='jackieBrown'
+//                   checked={this.state.selectedOption === 'jackieBrown'}
+//                   onChange={this.handleOptionChange}
+//                   className='form-check-input'
+//                 />
+//                 Jackie Brown
+//               </label>
+//             </div>
+
+//             <div className='form-check'>
+//               <label>
+//                 <input
+//                   type='radio'
+//                   name='answerOption'
+//                   value='killBill'
+//                   checked={this.state.selectedOption === 'killBill'}
+//                   onChange={this.handleOptionChange}
+//                   className='form-check-input'
+//                 />
+//                 Kill Bill
+//               </label>
+//             </div>
+
+//             <div className='form-check'>
+//               <label>
+//                 <input
+//                   type='radio'
+//                   name='answerOption'
+//                   value='hatefulEight'
+//                   checked={this.state.selectedOption === 'hatefulEight'}
+//                   onChange={this.handleOptionChange}
+//                   className='form-check-input'
+//                 />
+//                 Hateful Eight
+//               </label>
+//             </div>
+
+//             <div className='form-check'>
+//               <label>
+//                 <input
+//                   type='radio'
+//                   name='answerOption'
+//                   value='pulpFiction'
+//                   checked={this.state.selectedOption === 'pulpFiction'}
+//                   onChange={this.handleOptionChange}
+//                   className='form-check-input'
+//                 />
+//                 Pulp Fiction
+//               </label>
+//             </div>
+//             <div>
+//               <button type='submit'>Submit</button>
+//             </div>
+//           </form>
+//       </div>
+//     );
+//   }
+// }
+
+
+let movieList = ['Kill Bill', 'Pulp Fiction', 'Reservoir Dog'];
+
+let nextMovie = movieList.values() 
+
+class Quiz extends Component {
   constructor(props){
     super(props)
-    this.state = {
-      selectedOption: ''
-    };
-  }
-  //function to update current state
-  handleOptionChange = changeEvent => {
-    this.setState({
-      selectedOption: changeEvent.target.value
-    });
+      this.handleNextMovie = this.handleNextMovie.bind(this)
+      this.state = {
+        movie: 'My favorite Quentin Tarentino\'s movies are...'
+      }
   }
 
-
-  //function to submit change state and print answer result 
-  handleFormSubmit = formSubmitEvent => {
-    formSubmitEvent.preventDefault(); 
-    if(this.state.selectedOption === 'pulpFiction') {
-      return console.log('correct')
-    } else {
-      return console.log('incorrect')
-    }
+  handleNextMovie(){
+    this.setState((prevState) => {
+      return {
+        movie: nextMovie.next().value
+      }
+    })
   }
 
+  
   render() {
     return (
       <div>
+        <p>Hello from Quiz component!</p>
         <div>
-          <img className='imageStill' src='https://i2.wp.com/film-grab.com/wp-content/uploads/2011/02/223.png' alt='Pulp Fiction'></img>
+          {this.state.movie}
         </div>
-          <form onSubmit={this.handleFormSubmit}> 
-            <div className='form-check'>
-              <label>
-                <input
-                  type='radio'
-                  name='answerOption'
-                  value='jackieBrown'
-                  checked={this.state.selectedOption === 'jackieBrown'}
-                  onChange={this.handleOptionChange}
-                  className='form-check-input'
-                />
-                Jackie Brown
-              </label>
-            </div>
-
-            <div className='form-check'>
-              <label>
-                <input
-                  type='radio'
-                  name='answerOption'
-                  value='killBill'
-                  checked={this.state.selectedOption === 'killBill'}
-                  onChange={this.handleOptionChange}
-                  className='form-check-input'
-                />
-                Kill Bill
-              </label>
-            </div>
-
-            <div className='form-check'>
-              <label>
-                <input
-                  type='radio'
-                  name='answerOption'
-                  value='hatefulEight'
-                  checked={this.state.selectedOption === 'hatefulEight'}
-                  onChange={this.handleOptionChange}
-                  className='form-check-input'
-                />
-                Hateful Eight
-              </label>
-            </div>
-
-            <div className='form-check'>
-              <label>
-                <input
-                  type='radio'
-                  name='answerOption'
-                  value='pulpFiction'
-                  checked={this.state.selectedOption === 'pulpFiction'}
-                  onChange={this.handleOptionChange}
-                  className='form-check-input'
-                />
-                Pulp Fiction
-              </label>
-            </div>
-            <div>
-              <button type='submit'>Submit</button>
-            </div>
-          </form>
-      </div>
-    );
+        <button onClick={this.handleNextMovie}>Next</button>
+      </div> 
+    )
   }
 }
 
 
-
-
-export default Question;
+export default Quiz;
